8458e962ce7b2d07369702960c5d555c097aa9a6
sunil jat <108756403+Distraction5112@users.noreply.github.com>
2023-07-23 10:37:44 +0530

    Update dbConnect.js

--- Expense-tracker/dbConnect.js
+++ Expense-tracker/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 //pass=process.env.PASSWORD
-const url='mongodb+srv://suniljatiitism:gmihdZUMQ7rISnCs@expense-tracker.qzturxv.mongodb.net/?retryWrites=true&w=majority';
+const url='mongodb+srv://suniljatiitism:admin123@expense-tracker.qzturxv.mongodb.net/';
 mongoose.connect(url,{useUnifiedTopology: true});
 
 const connection=mongoose.connection;
